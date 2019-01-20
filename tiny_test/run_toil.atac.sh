fbcfc2a1f3345c5ad33ccb9a1bb71257672fcd1e
KerstenBreuer <kersten-breuer@outlook.com>
2019-01-20 19:22:37 +0100

    init

---
+++ tiny_test/run_toil.atac.sh
@@ -0,0 +1,29 @@
+BASEDIR=/scratch/cwl_working_dir/base
+OUTDIR=./test_run/
+WORKDIR=/scratch/cwl_working_dir/work
+TMPDIR=/scratch/cwl_working_dir/tmp
+TMPOUTDIR=/scratch/cwl_working_dir/tmp_out
+CWLSCRIPT=../CWL/workflows/ATACseq_pipeline.cwl
+INPUT=./test_main.atac.yml
+
+SINGULARITY_LOCALCACHEDIR=/scratch/.singularity/cache
+SINGULARITY_PULLFOLDER=/scratch/.singularity/pull
+SINGULARITY_TMPDIR=/scratch/.singularity/tmp
+export SINGULARITY_LOCALCACHEDIR
+export SINGULARITY_PULLFOLDER
+export SINGULARITY_TMPDIR
+
+if [ ! -d "$OUTDIR" ]
+then
+	mkdir "$OUTDIR"
+fi
+
+#cwltool --debug --singularity \
+#	--tmp-outdir-prefix $TMPOUTDIR --tmpdir-prefix $TMPDIR \
+#	--basedir $BASEDIR --outdir $OUTDIR \
+#	$CWLSCRIPT $INPUT
+#
+cwltoil --singularity --logDebug --clean always --cleanWorkDir always \
+	--tmp-outdir-prefix $TMPOUTDIR --tmpdir-prefix $TMPDIR \
+	 --workDir $WORKDIR --basedir $BASEDIR --outdir $OUTDIR \
+	$CWLSCRIPT $INPUT
